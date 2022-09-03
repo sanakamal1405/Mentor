f182bef2bca3d20e318ecb6c09451d5b6b7d124d
sanakamal <sanakamal1405@gmail.com>
2022-09-03 17:49:13 +0530

    Added Logging feature

--- src/SiteController.java
+++ src/SiteController.java
@@ -5,11 +5,26 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.*;
 
 
 @WebServlet("/SiteController")
 public class SiteController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static Logger l=Logger.getLogger(SiteController.class);
+	static {
+		try {
+			HTMLLayout layout=new HTMLLayout();
+			FileAppender appender=new FileAppender(layout,"C:/Trash/log.html",true);
+			l.addAppender(appender);
+			
+		}
+		catch(Exception e)
+		{
+			l.error("Problem while setting up log4j");
+			e.printStackTrace();
+		}
+	}
        
     
     public SiteController() {
@@ -31,11 +46,13 @@ public class SiteController extends HttpServlet {
 		String password = request.getParameter("password");
 		if(username.equals("sa") && password.contentEquals("sa"))
 		{
-	
+			l.info("Entered sigin after correct credentials");
 			response.sendRedirect("uploadfile.jsp");
 		}
-		else
+		else {
+			l.info("Returning to sign in page because of wrong credentials");
 			response.sendRedirect("login.jsp");
+		}
 	}
 
 }
